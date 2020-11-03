3a5bf4c15206f6f44123d667d9016b25a02f6c14
willjthorp <willthorp99@gmail.com>
2020-11-03 15:56:36 +0100

    Created docs app and components

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,19 +1,37 @@
+// ember-cli-build.js
 'use strict';
-/* global require, module */
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const isProduction = EmberAddon.env() === 'production';
+
+const purgeCSS = {
+  module: require('@fullhuman/postcss-purgecss'),
+  options: {
+    content: [
+      // add extra paths here for components/controllers which include tailwind classes
+      // './app/index.html',
+      // './app/templates/**/*.hbs'
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  }
+}
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    // Add options here
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: require('postcss-import'),
+            options: {
+              path: ['node_modules']
+            }
+          },
+          require('tailwindcss')('./tailwind.config.js'),
+          ...isProduction ? [purgeCSS] : []
+        ]
+      }
+    }
   });
-
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
-
   return app.toTree();
 };
--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -46,6 +46,8 @@ module.exports = function(environment) {
   if (environment === 'production') {
     ENV.locationType = 'hash';
     ENV.rootURL = '/ember-tracked-local-storage/';
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/ember-tracked-local-storage/';
     // here you can enable a production-specific feature
   }
 
