de069cb65a60b928ff312aade91c51649ef37769
willjthorp <willthorp99@gmail.com>
2021-03-03 15:26:12 +0100

    Exported util functions in app

--- app/controllers/application.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-tracked-local-storage/controllers/application';
---
+++ app/utils/tracked-in-local-storage.js
@@ -0,0 +1,6 @@
+export {
+  default,
+  localStorageGet,
+  localStorageSet,
+  trackedInLocalStorage,
+} from 'ember-tracked-local-storage/utils/tracked-in-local-storage';
